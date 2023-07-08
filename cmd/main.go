75bb28800979f0c01198fafecefb3047a264ef22
laches1sm <rebeccalmartin16@gmail.com>
2023-07-09 00:09:46 +0100

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"github.com/KnutZuidema/golio"
+    "github.com/KnutZuidema/golio/api"
+	log "github.com/sirupsen/logrus"
+	"encoding/json"
+	"kayn-form/models"
+
+func main(){
+	// we need to: get user region 
+	// username
+	// map supplied user region to a valid Riot API region (sorry tencent users)
+
+}
